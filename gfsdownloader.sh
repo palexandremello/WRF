85a65808ab96f992f3d4473942c12fec5d318059
Paulo Alexandre <palexandremello@gmail.com>
2018-04-09 10:18:43 -0300

    Script

---
+++ gfsdownloader.sh
@@ -0,0 +1,55 @@
+
+anoi=`date  +%Y`
+anof=$anoi
+#mesi=`date "-d -6 month" +%m` 
+mesi=03 
+mesf=$mesi
+diai=30
+diaf=31
+utci=00
+fc=000
+fcf=024
+
+i=0
+j=0
+k=0
+m=0	
+s=0
+int=0600
+
+cd $HOME
+source .bashrc
+
+cd $HOME/WRF/DATA
+
+if [ ! -e $HOME/WRF/DATA/$anoi$mesi$diai$fcf ]
+then 
+
+mkdir $HOME/WRF/DATA/$anoi$mesi$diai$fcf
+
+fi
+
+cd $HOME/WRF/DATA/$anoi$mesi$diai$fcf
+
+eval ' 
+
+for j in {'"`date +%Y`"'..'"`date +%Y`"'..1}
+
+do
+       for i in {'"`date +%m`"'..'"`date +%m`"'..1}
+ 	do
+		for k in {'"`date "-d -24 hours" +%d`"'..'"`date "-d -24 hours" +%d`"'..1}
+		do       
+      
+          		for s in {'"$fc"'..'"$fcf"'..003}
+                        do 
+ 
+
+ 		curl -v -O ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${anoi}${mesi}${diai}${utci}/gfs.t${utci}z.pgrb2.0p50.f${s}
+
+
+                    done                  	       
+		        done       
+	        done
+        done
+'
